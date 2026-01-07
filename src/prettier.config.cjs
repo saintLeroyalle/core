fbbebde8c0074009d4d643eb94132127b03f7326
Leroyalle <nikolajmelonov@yandex.ru>
2026-01-07 12:40:06 +0300

    fix(prettier): format

--- src/prettier.config.cjs
+++ src/prettier.config.cjs
@@ -1,23 +1,22 @@
-module.exports ={
-  "tabWidth": 2,
-  "singleQuote": true,
-  "trailingComma": "all",
-  "semi": true,
-  "bracketSameLine": true,
-  "printWidth": 100,
-  "arrowParens": "avoid",
-  "importOrder": [
-    "<THIRD_PARTY_MODULES>",
-    "^@nestjs/(.*)$",
-    "^@prisma/(.*)$",
-    "^@/(.*)$",
-    "^\\.\\./(.*)$",
-    "^\\./(.*)$"
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  bracketSameLine: true,
+  printWidth: 100,
+  arrowParens: 'avoid',
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^@nestjs/(.*)$',
+    '^@prisma/(.*)$',
+    '^@/(.*)$',
+    '^\\.\\./(.*)$',
+    '^\\./(.*)$',
   ],
-  "importOrderSeparation": true,
-  "importOrderSortSpecifiers": true,
-  "importOrderCaseInsensitive": true,
-  "importOrderParserPlugins": ["classProperties", "decorators-legacy", "typescript"],
-  "plugins": ["@trivago/prettier-plugin-sort-imports"]
-
-}
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  importOrderParserPlugins: ['classProperties', 'decorators-legacy', 'typescript'],
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
